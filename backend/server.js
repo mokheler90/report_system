9b41daad431456bb897eb8c576cd400366ce86a1
Retselisitsoe Mokhele <retselisitsoemokhele737@gmail.com>
2025-10-15 13:25:21 +0200

    Final

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const pool = new Pool({
   port: process.env.DB_PORT || 5432,
   database: process.env.DB_NAME || 'luct_system',
   user: process.env.DB_USER || 'postgres',
-  password: process.env.DB_PASSWORD || 'emmanuel^',
+  password: process.env.DB_PASSWORD || 'emmanuel5^',
   max: 20,
   idleTimeoutMillis: 30000,
   connectionTimeoutMillis: 5000,
